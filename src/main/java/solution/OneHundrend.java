7612e20b72dd05965aebb2710eda8bb27113eb67
liulu <liulu@shuidihuzhu.com>
2019-02-18 01:40:55 +0800

    02/18

--- src/main/java/solution/OneHundrend.java
+++ src/main/java/solution/OneHundrend.java
@@ -1,9 +1,9 @@
 package solution;
 
+
 import org.dora.algorithm.datastructe.ListNode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author liulu
@@ -94,7 +94,28 @@ public class OneHundrend {
      * @return
      */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int imin = 0, imax = m, max_left = 0, min_right = 0;
+        while (imin <= imax) {
+            int i = (imin + imax) / 2;
+            int j = (m + n + 1) / 2 - i;
+            if (i > 0 && nums1[i - 1] > nums2[j]) {
+                imax = i - 1;
+            } else if (i < m && nums1[i] < nums2[j - 1]) {
+                imin = i + 1;
+            } else {
+                if (i == m) {
+                    max_left = nums1[i - 1];
+                } else if (i == 0) {
+                    max_left = nums2[j - 1];
+                }
+            }
+        }
         return -1;
+
     }
 
     /**
@@ -204,6 +225,140 @@ public class OneHundrend {
             idx++;
         }
         return sign * ans.intValue();
+    }
+
+    /**
+     * 9. Palindrome Number
+     *
+     * @param x
+     * @return
+     */
+    public boolean isPalindrome(int x) {
+        if (x == 0) {
+            return true;
+        }
+        if (x % 10 == 0) {
+            return false;
+        }
+        int sum = 0;
+        while (x > sum) {
+            sum = sum * 10 + x % 10;
+            x /= 10;
+        }
+        return sum == x || sum / 10 == x;
+    }
+
+    /**
+     * 10. Regular Expression Matching
+     *
+     * @param s
+     * @param p
+     * @return
+     */
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = p.charAt(j - 1) == '*' && dp[0][j - 2];
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.') {
+                        dp[i][j] = dp[i][j - 2];
+                    } else {
+                        dp[i][j] = dp[i][j - 1] || dp[i][j - 2] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    /**
+     * 11. Container With Most Water
+     *
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int result = 0, left = 0, right = height.length - 1;
+        while (left < right) {
+            result = Math.max(result, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 14. Longest Common Prefix
+     *
+     * @param strs
+     * @return
+     */
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+
+    /**
+     * 15. 3Sum
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1, right = nums.length - 1, target = 0 - nums[i];
+            while (left < right) {
+                if (nums[left] + nums[right] == target) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (nums[left] + nums[right] < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return ans;
     }
 
 
