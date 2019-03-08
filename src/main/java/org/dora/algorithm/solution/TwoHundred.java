b18dca249a333080156919277914acfe3c99a0dd
liulu <liulu@shuidihuzhu.com>
2019-03-09 03:06:30 +0800

    03/09

--- src/main/java/org/dora/algorithm/solution/TwoHundred.java
+++ src/main/java/org/dora/algorithm/solution/TwoHundred.java
@@ -87,11 +87,106 @@ public class TwoHundred {
 
         while (!list.isEmpty()) {
             int size = list.size();
-            List<Integer> tmp = new ArrayList<>();
+            LinkedList<Integer> tmp = new LinkedList<>();
             for (int i = 0; i < size; i++) {
+                TreeNode node = list.pollFirst();
+                if (!isLeft) {
+                    tmp.addLast(node.val);
+                } else {
+                    tmp.addFirst(node.val);
+                }
+                if (node.left != null) {
+                    list.addLast(node.left);
+                }
+                if (node.right != null) {
+                    list.addLast(node.right);
+                }
             }
+            isLeft = !isLeft;
+            ans.add(tmp);
         }
         return ans;
     }
+
+    /**
+     * 104. Maximum Depth of Binary Tree
+     *
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    /**
+     * 105. Construct Binary Tree from Preorder and Inorder Traversal
+     *
+     * @param preorder
+     * @param inorder
+     * @return
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || inorder == null) {
+            return null;
+        }
+        if (preorder.length == 0 || inorder.length == 0) {
+            return null;
+        }
+        return buildTree(0, 0, inorder.length - 1, preorder, inorder);
+    }
+
+    private TreeNode buildTree(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+        if (preStart > preorder.length || inStart > inEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int idx = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == root.val) {
+                idx = i;
+                break;
+            }
+        }
+        root.left = buildTree(preStart + 1, inStart, idx - 1, preorder, inorder);
+        root.right = buildTree(preStart + idx - inStart + 1, idx + 1, inEnd, preorder, inorder);
+        return root;
+    }
+
+    /**
+     * 106. Construct Binary Tree from Inorder and Postorder Traversal
+     *
+     * @param inorder
+     * @param postorder
+     * @return
+     */
+    public TreeNode buildTreeII(int[] inorder, int[] postorder) {
+        if (inorder == null || postorder == null) {
+            return null;
+        }
+        return buildTreePost(0, inorder.length - 1, postorder.length - 1, inorder, postorder);
+    }
+
+    private TreeNode buildTreePost(int inStart, int inEnd, int postEnd, int[] inorder, int[] postorder) {
+        if (inStart > inEnd || postEnd < 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(postorder[postEnd]);
+        int idx = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == root.val) {
+                idx = i;
+                break;
+            }
+        }
+        int rightCount = inorder.length - idx;
+        int rightLeft = idx - inStart;
+        root.left = buildTreePost(inStart, idx - 1, postEnd - 1 - rightCount, inorder, postorder);
+        root.right = buildTreePost(idx + 1, inEnd, postEnd - 1, inorder, postorder);
+        return root;
+    }
+
 
 }
