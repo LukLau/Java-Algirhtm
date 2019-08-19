c3e8ce7b73f5ceb8a37a61ec6fcffef494b4c8ef
lauluk <740491884@qq.com>
2019-08-20 01:46:24 +0800

    leetcode v2版本

---
+++ src/main/java/org/dora/algorithm/solution/v2/OneHundred.java
@@ -0,0 +1,234 @@
+package org.dora.algorithm.solution.v2;
+
+import org.dora.algorithm.datastructe.ListNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author dora
+ * @date 2019-08-19
+ */
+public class OneHundred {
+
+    /**
+     * 10. Regular Expression Matching
+     *
+     * @param s
+     * @param p
+     * @return
+     */
+    public boolean isMatch(String s, String p) {
+        if (s == null) {
+            return true;
+        }
+        if (p == null) {
+            return false;
+        }
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = p.charAt(j - 1) == '*' && dp[0][j - 2];
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.') {
+                        dp[i][j] = dp[i][j - 2];
+                    } else {
+                        dp[i][j] = dp[i][j - 1] || dp[i - 1][j] || dp[i][j - 2];
+                    }
+
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+
+    /**
+     * 11. Container With Most Water
+     *
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int result = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            result = Math.max(result, (right - left) * Math.min(height[left], height[right]));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 12. Integer to Roman
+     *
+     * @param num
+     * @return
+     */
+    public String intToRoman(int num) {
+        if (num <= 0) {
+            return "";
+        }
+        return "";
+    }
+
+    /**
+     * 14. Longest Common Prefix
+     *
+     * @param strs
+     * @return
+     */
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+
+    }
+
+    /**
+     * 15. 3Sum
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new ArrayList<>();
+        }
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int val = nums[i] + nums[left] + nums[right];
+                if (val == 0) {
+                    List<Integer> tmp = Arrays.asList(nums[i], nums[left], nums[right]);
+                    ans.add(tmp);
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (val < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return ans;
+    }
+
+
+    /**
+     * 16. 3Sum Closest
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int threeSumClosest(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return Integer.MAX_VALUE;
+        }
+        int result = nums[0] + nums[1] + nums[2];
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int val = nums[i] + nums[left] + nums[right];
+                if (val == target) {
+                    return val;
+                }
+                if (val < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+                if (Math.abs(val - target) < Math.abs(result - target)) {
+                    result = val;
+                }
+
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * 17. Letter Combinations of a Phone Number
+     *
+     * @param digits
+     * @return
+     */
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) {
+            return new ArrayList<>();
+        }
+        String[] map = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        LinkedList<String> deque = new LinkedList<>();
+        deque.add("");
+        for (int i = 0; i < digits.length(); i++) {
+            int index = Character.getNumericValue(digits.charAt(i));
+            String value = map[index];
+            while (deque.peek().length() == i) {
+                String pop = deque.pop();
+                for (Character tmp : value.toCharArray()) {
+                    deque.add(pop + tmp);
+                }
+            }
+        }
+        return deque;
+    }
+
+    /**
+     * 19. Remove Nth Node From End of List
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+
+
+    }
+
+}
