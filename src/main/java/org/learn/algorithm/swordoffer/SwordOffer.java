afaf08c02bee8b6a23c2ba6ed0d0da80139c0b8b
luk <740491884@qq.com>
2021-05-12 07:57:24 +0800

    feat: 05/13

--- src/main/java/org/learn/algorithm/swordoffer/SwordOffer.java
+++ src/main/java/org/learn/algorithm/swordoffer/SwordOffer.java
@@ -2,11 +2,10 @@ package org.learn.algorithm.swordoffer;
 
 import org.learn.algorithm.datastructure.ListNode;
 import org.learn.algorithm.datastructure.TreeNode;
-import org.springframework.util.ResourceUtils;
 
-import java.awt.peer.WindowPeer;
-import java.util.AbstractCollection;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * @author luk
@@ -122,6 +121,98 @@ public class SwordOffer {
             }
         }
         return array[left];
+    }
+
+
+    public boolean IsPopOrder(int[] pushA, int[] popA) {
+        if (pushA == null || popA == null) {
+            return false;
+        }
+        Stack<Integer> stack = new Stack<>();
+        int j = 0;
+        for (int i = 0; i < pushA.length; i++) {
+            stack.push(i);
+            while (!stack.isEmpty() && pushA[stack.peek()] == popA[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        ArrayList<Integer> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+
+            TreeNode poll = queue.poll();
+            result.add(poll.val);
+            if (poll.left != null) {
+                queue.offer(poll.left);
+            }
+            if (poll.right != null) {
+                queue.offer(poll.right);
+            }
+        }
+        return result;
+    }
+
+
+    public boolean VerifySequenceOfBST(int[] sequence) {
+        if (sequence == null || sequence.length == 0) {
+            return false;
+        }
+        return intervalVerify(sequence, 0, sequence.length - 1);
+    }
+
+    private boolean intervalVerify(int[] sequence, int start, int end) {
+        if (start == end) {
+            return true;
+        }
+        if (start > end) {
+            return false;
+        }
+        int leftIndex = start;
+        while (leftIndex < end && sequence[leftIndex] < sequence[end]) {
+            leftIndex++;
+        }
+        int rightIndex = leftIndex;
+        while (rightIndex < end && sequence[rightIndex] > sequence[end]) {
+            rightIndex++;
+        }
+        if (rightIndex != end) {
+            return false;
+        }
+        return intervalVerify(sequence, start, leftIndex - 1) && this.intervalVerify(sequence, leftIndex, end - 1);
+    }
+
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        intervalPath(result, new ArrayList<>(), root, target);
+        return result;
+    }
+
+    private void intervalPath(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> tmp, TreeNode root, int target) {
+        tmp.add(root.val);
+        if (root.left == null && root.right == null && root.val == target) {
+            result.add(new ArrayList<>(tmp));
+            return;
+        } else {
+            if (root.left != null) {
+                intervalPath(result, tmp, root.left, target - root.val);
+            }
+            if (root.right != null) {
+                intervalPath(result, tmp, root.right, target - root.val);
+            }
+        }
+        tmp.remove(tmp.size() - 1);
     }
 
 
